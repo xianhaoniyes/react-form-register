0b01fb398dcb3fff4b3e20d7f3b6c44d6be3f18a
xianhaoni <xianhaoni@gmail.com>
2020-06-25 14:54:17 +0800

    validation function finished

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ import { ERRORS } from './constants';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+    };
+  }
+
+  onInput = (field) => event => {
+    if(!(ERRORS[field].regex).test(event.target.value)&&event.target.value.length>0)
+      this.setState({
+        [field+"Error"]: ERRORS[field].message
+      });
+    else
+      this.setState({
+        [field+"Error"]: null
+      });
+  }
+
+  onSubmit = () => {
+    if(this.state.usernameError===null &this.state.passwordError===null)
+      alert("Success");
   }
 
   render() {
@@ -16,7 +34,7 @@ export default class App extends React.Component {
             width="100px"
           />
         </div>
-        <form noValidate>
+        <form>
           <label for="username">
             <span className="label-star">*</span> Username:
             <input
@@ -24,9 +42,10 @@ export default class App extends React.Component {
               name="username"
               placeholder="6~10 characters"
               type="text"
-              noValidate
+              onChange={this.onInput("username")}
             />
           </label>
+          {this.state.usernameError&&<p className="error">{this.state.usernameError}</p>}
           <label for="password">
             <span className="label-star">*</span> Password:
             <input
@@ -34,10 +53,11 @@ export default class App extends React.Component {
               name="password"
               placeholder="6~10 numbers"
               type="password"
-              noValidate
+              onChange={this.onInput("password")}
             />
           </label>
-          <button type="submit">Register</button>
+            {this.state.passwordError&&<p className="error">{this.state.passwordError}</p>}
+          <button type="submit" onClick={this.onSubmit}>Register</button>
         </form>
       </div>
     );
